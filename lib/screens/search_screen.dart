043e11084a61e198b1c22e780a63a626e1b9756d
Ashwini Singh <ashwinidotx@gmail.com>
2023-08-01 13:08:21 +0530

    Kermit 2

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,10 +3,38 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget {
+  String? _search;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Search Screen'),
+      child: TextFormField(
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(labelText: "Search"),
+        validator: (value) {
+          if (value?.isEmpty ?? true) {
+            return 'Khaali kyu chora';
+          }
+          return null;
+        },
+        onSaved: (value)=>_search=value,
+      ),
     );
   }
 }
+
+
+/*
+
+TextFormField(
+              decoration: InputDecoration(labelText: "Email"),
+              validator: (value) {
+                if (value?.isEmpty ?? true) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (value) => _email = value,
+            ),
+
+
+* */
